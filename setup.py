c1c8b4a66a3ffe286f34a65697434e0f655c31a3
128 <42086377+nano128@users.noreply.github.com>
2018-09-04 09:46:40 +0200

    some more fields

--- setup.py
+++ setup.py
@@ -4,7 +4,16 @@ from setuptools import setup, Extension
 def try_build():
     setup(
         name="nano_dpow",
+        version='0.0.1',
+        description='write a description',
+        url='https://github.com/jamescoxon/nano_distributed_pow_client',
+        author='James',
+        author_email='add email',
         scripts=['client.py'],
+        license='MIT',
+        python_requires='>=3.0',
+        install_requires=[
+        'websocket-client'],
         ext_modules=[
             Extension(
                 'mpow',
