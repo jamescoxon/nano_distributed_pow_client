8f3e92d697a9f75e4c9c959c5de51ec94928509a
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-10-23 14:11:00 +0100

    Fix performance_check.py

--- performance_check.py
+++ performance_check.py
@@ -1,9 +1,24 @@
-import client
 import argparse
 from timeit import timeit
 from sys import exit
+import requests, json
+import mpow
 
 HASH = 'C876D20ECA5D4F8C7BD06B55FEFEABB46117A6BB3825ED95BEFC33B5CCB3316D'
+
+WORK_SERVER = 'http://%s:%s' % ('127.0.0.1', '7070')
+NANO_THRESHOLD = 'ffffffc000000000'
+
+def get_work_lib(from_hash, threshold):
+    work = format(mpow.generate(bytes.fromhex(from_hash), int(threshold, 16)), '016x')
+    return work
+
+def get_work_node(from_hash, threshold):
+    get_work = '{ "action" : "work_generate", "hash" : "%s", "threshold": "%s", "use_peers": "true" }' % ( from_hash, threshold )
+    r = requests.post(WORK_SERVER, data = get_work)
+    resulting_work = r.json()
+    work = resulting_work['work'].lower()
+    return work
 
 def wrapper(func, *args, **kwargs):
     def wrapped():
@@ -19,19 +34,19 @@ if __name__ == "__main__":
 
     # try one of each, to check if they are available
     try:
-        client.get_work_node(HASH)
+        get_work_node(HASH, NANO_THRESHOLD)
     except Exception as e:
         print('Error getting work from the node, wont proceed: {}'.format(e))
         exit(1)
 
     try:
-        client.get_work_lib(HASH)
+        get_work_lib(HASH, NANO_THRESHOLD)
     except Exception as e:
         print('Error getting work from the library, wont proceed: {}'.format(e))
         exit(1)
 
-    node_wrapped = wrapper(client.get_work_node, HASH)
-    lib_wrapped = wrapper(client.get_work_lib, HASH)
+    node_wrapped = wrapper(get_work_node, HASH, NANO_THRESHOLD)
+    lib_wrapped = wrapper(get_work_lib, HASH, NANO_THRESHOLD)
 
     print('Computing {} works using the node.'.format(args.works))
     node_time = timeit(node_wrapped, number=args.works)
