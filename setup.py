3da5d7284d64a6d9b5e168ba0ad03a6fd2678ee9
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-12-20 20:31:20 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(
     name="nano-dpow-client",
-    version='0.0.1',
+    version='1.1.0',
     description='Work client for the Nano (cryptocurrency) Distributed Proof of Work System. Supports CPU and GPU.',
     url='https://github.com/jamescoxon/nano_distributed_pow_client',
     author='James Coxon',
